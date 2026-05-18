344f037043666f01b8bd498e24b28950a8cbc50d
behzad <behzad@MacBook-Air.local>
2026-05-18 15:38:13 -0700

    add CANBUS_Simulator function to convert all input/output datas to CAN Frames and print them

--- EAE_Coding.c
+++ EAE_Coding.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #define TEMP_LOW_C              35.0f
 #define TEMP_MEDIUM_C           45.0f
@@ -22,6 +23,10 @@
 #define FAN_SPEED_LOW           30U
 #define FAN_SPEED_MEDIUM        60U
 #define FAN_SPEED_HIGH          100U
+
+#define CAN_INPUT_FRAME_ID      0x100U
+#define CAN_OUTPUT_FRAME_ID     0x101U
+#define CAN_FRAME_DATA_SIZE     8U
 
 typedef struct
 {
@@ -114,6 +119,45 @@ void run_cooling_logic(const Inputs_t *inputs, Outputs_t *outputs)
 }
 
 /**
+ * @brief Converts cooling inputs and outputs to simulated CAN frames.
+ */
+static void CANBUS_Simulator(const Inputs_t *inputs, const Outputs_t *outputs)
+{
+    int16_t coolant_temp_deci_c = (int16_t)(inputs->coolant_temp_c * 10.0f);
+
+    uint8_t received_frame[CAN_FRAME_DATA_SIZE] =
+        {   //Can bus data length is 8 bytes and receiving frames are in the following order:
+            (uint8_t)(inputs->ignition_on ? 1 : 0),
+            (uint8_t)(coolant_temp_deci_c & 0xFF),
+            (uint8_t)((coolant_temp_deci_c >> 8) & 0xFF),
+            0,0,0,0,0
+        };
+        
+        //Can bus data length is 8 bytes and Sending frames are in the following order:
+    uint8_t send_frame[CAN_FRAME_DATA_SIZE] =
+        {
+            (uint8_t)outputs->pump_speed_percent,
+            (uint8_t)outputs->fan_speed_percent,
+            (uint8_t)(outputs->safety_shutdown ? 1 : 0),
+            0,0,0,0,0
+        };
+
+    printf("CAN Received: [0x%03X", CAN_INPUT_FRAME_ID); // First print Id then all 8 bytes -- all frames converting to hex
+    for ( int i = 0; i < CAN_FRAME_DATA_SIZE; i++)
+    {
+        printf(" 0x%02X", received_frame[i]);
+    }
+
+    printf("]\nCAN Send:     [0x%03X", CAN_OUTPUT_FRAME_ID);
+    for ( int i = 0; i < CAN_FRAME_DATA_SIZE; i++)
+    {
+        printf(" 0x%02X", send_frame[i]);
+    }
+
+    printf("]\n");
+}
+
+/**
  * @brief Prints cooling system status.
  */
 static void print_status(const Inputs_t *inputs, const Outputs_t *outputs)
@@ -123,19 +167,20 @@ static void print_status(const Inputs_t *inputs, const Outputs_t *outputs)
     printf("Pump Speed: %u%%\n", outputs->pump_speed_percent);
     printf("Fan Speed: %u%%\n", outputs->fan_speed_percent);
     printf("Safety Shutdown: %s\n", outputs->safety_shutdown ? "ACTIVE" : "INACTIVE");
-    printf("-----------------------------\n");
+    CANBUS_Simulator(inputs, outputs);
+    printf("-----------------------------------------------------------\n");
 }
 
 int main(void)
 {
     Inputs_t test_cases[] =
     {
-        {false, 25.0f},
-        {true, 30.0f},
-        {true, 38.0f},
-        {true, 50.0f},
-        {true, 65.0f},
-        {true, 80.0f}
+        {false,25},
+        {true, 30},
+        {true, 38},
+        {true, 50},
+        {true, 65},
+        {true, 80}
     };
 
     Outputs_t outputs;
